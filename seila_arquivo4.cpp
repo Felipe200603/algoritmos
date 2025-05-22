417f51e57e6a53a1e9a2a5386966ab4e8ac2b437
Felipe200603 <felipe.leal@escolar.ifrn.edu.br>
2025-05-22 22:12:29 +0000

    Salvando o status atual

--- seila_arquivo4.cpp
+++ seila_arquivo4.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int main() {
+    float valordaCompra;
+    int prestacoes;
 
+    cout << "Escreva o Valor da Compra: R$ ";
+    cin >> valordaCompra;
+    cout << "Escreva a Quantidade de Prestações";
+    cin >> prestacoes;
 
+    float avista = valordaCompra * 0.9;
+    float aprazo = valordaCompra * 1.2;
+    float valorPrestacao = aprazo / prestacoes;
 
+    cout << "Valor a vista (10% de desconto): R$ " << avista << endl;
+    cout << "Valor a prazo (20% de juros): R$ " << aprazo << endl;
+    cout << "Valor de Cada Prestação: R$ " << valorPrestacao << endl;
 
 return 0;   
 }
